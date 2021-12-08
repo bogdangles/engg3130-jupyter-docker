e9440fb79ec6627b513ad53b1140ad2a704ffe9e
jbest <jbest@uoguelph.ca>
2021-12-08 11:25:40 -0500

    Tag images with date built

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+DATE=$(date +%Y-%m-%dT%H%M%S%z)
 # This will fail unless you are logged into DockerHub
 # Use this command to login: docker login --username=joelbest
-docker build . -t joelbest/engg3130-jupyter-docker:latest --no-cache || exit
+docker build . -t joelbest/engg3130-jupyter-docker:latest -t joelbest/engg3130-jupyter-docker:$DATE --no-cache || exit
 docker push joelbest/engg3130-jupyter-docker:latest
+docker push joelbest/engg3130-jupyter-docker:$DATE
